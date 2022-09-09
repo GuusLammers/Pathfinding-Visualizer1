c6e66271c53e61cb47a6d2cb8e5b5fb63adf56e1
GuusLammers <guuslammers16@gmail.com>
2022-09-09 14:47:28 -0700

    Refactored repository

--- src/App.tsx
+++ src/App.tsx
@@ -17,8 +17,8 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => setCount((count) => count + 10)}>
+          count is fuck
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
